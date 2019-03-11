713182bd4fc6bae530a02bbe7e82df718190405a
Eugene Khristo <marteau9292@gmail.com>
2019-03-11 08:01:06 +0300

    basic react-spring transition

--- level-two-react-levelup/src/App.js
+++ level-two-react-levelup/src/App.js
@@ -1,7 +1,17 @@
 import React, { Component, Fragment } from 'react';
+import { Transition } from 'react-spring/renderprops';
 import './App.css';
 import { Toggle } from 'Utils';
-import { Modal } from 'Elements';
+// import { Modal } from 'Elements';
+
+const Heading = props => {
+  return (
+    <Fragment>
+      <h2 style={props}>Hello</h2>
+      <h3 style={props}>{Math.round(props.value)}</h3>
+    </Fragment>
+  );
+};
 
 class App extends Component {
   render() {
@@ -10,11 +20,15 @@ class App extends Component {
         <Toggle>
           {({ on, handleToggle }) => (
             <Fragment>
-              <button onClick={handleToggle}>Login</button>
-              <Modal on={on} onToggle={handleToggle}>
-                <h2>Children of Modal</h2>
-                <p>Just some text</p>
-              </Modal>
+              <button onClick={handleToggle}>Show / hide</button>
+              <Transition
+                items={on}
+                from={{ opacity: 0, value: 0 }}
+                leave={{ opacity: 0, value: 0 }}
+                enter={{ opacity: 1, value: 100 }}
+              >
+                {on => on && Heading}
+              </Transition>
             </Fragment>
           )}
         </Toggle>
@@ -24,3 +38,13 @@ class App extends Component {
 }
 
 export default App;
+
+// {({ on, handleToggle }) => (
+//   <Fragment>
+//     <button onClick={handleToggle}>Login</button>
+//     <Modal on={on} onToggle={handleToggle}>
+//       <h2>Children of Modal</h2>
+//       <p>Just some text</p>
+//     </Modal>
+//   </Fragment>
+// )}
