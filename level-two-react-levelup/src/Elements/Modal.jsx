6725924a4a2a698a7b80dd3a5cad62194c56fe7e
Eugene <eugene.khristo@mifort.org>
2019-03-12 16:24:29 +0300

    Basic transition with Spring for modal component

--- level-two-react-levelup/src/Elements/Modal.jsx
+++ level-two-react-levelup/src/Elements/Modal.jsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import Icon from './Icon';
 import { Card } from './Cards';
 import { colors } from 'Utils';
+// spring
+import { Transition } from 'react-spring/renderprops';
 
 class Modal extends Component {
   render() {
@@ -12,14 +14,27 @@ class Modal extends Component {
     return (
       <Portal>
         {on && (
-          <Overlay onClick={onToggle}>
-            <ModalCard>
-              <CloseBtn onClick={onToggle}>
-                <Icon name="close" color={colors.yellow} />
-              </CloseBtn>
-              {children}
-            </ModalCard>
-          </Overlay>
+          <Transition
+            items={on}
+            from={{opacity: 0, bgPpacity: 0, y: -25 }}
+            enter={{opacity: 1, bgPpacity: 0.5, y: 0 }}
+            leave={{opacity: 0, bgPpacity: 0, y: -25 }}
+          >
+            {on =>
+              on &&
+              (styles => (
+                <Overlay onClick={onToggle}>
+                  <ModalCard style={{opacity: styles.opacity, transform: `translate3D(0,${styles.y}px,0)`}}>
+                    <CloseBtn onClick={onToggle}>
+                      <Icon name="close" color={colors.black} />
+                    </CloseBtn>
+                    {children}
+                  </ModalCard>
+                  <Background style={{opacity: styles.bgPpacity}} />
+                </Overlay>
+              ))
+            }
+          </Transition>
         )}
       </Portal>
     );
@@ -32,16 +47,25 @@ const Overlay = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const Background = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+`;
+
 const ModalCard = styled(Card)`
   position: relative;
   min-width: 320px;
-  transform: translateY(-15vh);
+  /* transform: translateY(-15vh); */
+  z-index: 1000;
 `;
 
 const CloseBtn = styled.button`
